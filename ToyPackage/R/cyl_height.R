895556ece047257419723a93a2467a2d864a057c
danielpetterson <dpet922@hotmail.com>
2022-07-28 13:47:30 +0100

    Formatting

--- ToyPackage/R/cyl_height.R
+++ ToyPackage/R/cyl_height.R
@@ -3,10 +3,11 @@
 #' @param radius Any number greater than zero
 #' @param volume Any number greater than zero
 #' @return Volume divided by the product of \code{radius} and \pi.
+#'
 #' @examples
 #' cyl_height(2,25)
 #' cyl_height(5,1)
-#' 
+#'
 
 cyl_height <- function(radius, volume){
   height <- volume / (radius * pi)
