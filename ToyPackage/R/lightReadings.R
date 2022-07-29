6a6ab7b38b7dfb710c8bdd5a42278220ac510449
danielpetterson <dpet922@hotmail.com>
2022-07-29 02:46:28 +0100

    Create lightReadings.R

---
+++ ToyPackage/R/lightReadings.R
@@ -0,0 +1,12 @@
+#' Geolocator light intensity readings
+#'
+#' Light intensity measurements from a light logger commonly used for geolocation.
+#'
+#' @usage data(lightReadings)
+#'
+#' @format A data frame with 22320 rows and 2 variables:
+#' \describe{
+#'   \item{datetime}{A time series every two minutes from 2010-07-21 00:01:00}
+#'   \item{light}{Light intensity measurements}
+#' }
+"lightReadings"
