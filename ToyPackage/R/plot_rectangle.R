8240bf598998f372c46092bcf56050ce8168d1db
danielpetterson <dpet922@hotmail.com>
2022-07-29 02:45:11 +0100

    fixed import of ggplot2 issue

--- ToyPackage/R/plot_rectangle.R
+++ ToyPackage/R/plot_rectangle.R
@@ -2,22 +2,21 @@
 #'
 #' @param height A number
 #' @param width A number
-#' @return Visualisation of a rectangle of the given height and width
-#'
+#' @return Plots a rectangle of the given height and width.
+#' @import ggplot2
+#' @export
 #' @examples
 #' plot_rectangle(2,3)
 #' plot_rectangle(5,3)
 #'
 
-require(ggplot2)
-
 plot_rectangle <- function(height, width){
   x <- c(0,height,height,0,0)
   y <- c(0,0,width,width,0)
   data <- data.frame(x,y)
-  p <- ggplot(data) +
-    aes(x,y) +
-    geom_path()
+  p <- ggplot2::ggplot(data) +
+    ggplot2::aes(x,y) +
+    ggplot2::geom_path()
   return(p)
 }
 
